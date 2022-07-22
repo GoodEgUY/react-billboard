e46fc4138db1d78dc363a3054928f9bb7c260db8
Pugga <aezakmi2508@gmail.com>
2022-07-22 13:36:34 +0300

    Added validation

--- src/components/Pop-up/Popup.jsx
+++ src/components/Pop-up/Popup.jsx
@@ -3,19 +3,22 @@ import "./popup.css"
 
 
 function Popup(props) {
-const [name,setName] = React.useState();
-const [phone, setPhone] = React.useState();
+    const [name, setName] = React.useState();
+    const [phone, setPhone] = React.useState();
     function saveInfo() {
-        alert(`${name},обратный звонок заказан на номер: ${phone}`);
+        if (name.length && phone.length <= 2) {
+            alert("Введите корректные данный");
+        } else {
+            alert(`${name},обратный звонок заказан на номер: ${phone}`);
+        }
     }
     return (
         <div className="popupWrapper">
             <div className="popupBody">
                 <h2>Заполните информацию</h2>
-
                 <form>
-                    <input type="text" placeholder="Имя" onChange={e => setName(e.target.value)}/>
-                    <input type="text" placeholder="Телефон" onChange={ e => setPhone(e.target.value)} />
+                    <input type="text" placeholder="Имя" onChange={e => setName(e.target.value)} />
+                    <input type="phone" placeholder="Телефон" onChange={e => setPhone(e.target.value)} />
                     <button onClick={saveInfo} className="popupBtn">Сохранить</button>
                 </form>
                 <img src="./images/cross.png" className="popupClose" onClick={props.closePopup} alt="Close" />
