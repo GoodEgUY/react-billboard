e04f9b2268e83efa2b277de5a36ffa487fd0160b
Mike <mykhailo.ovsiannykov@isquad.dev>
2022-07-24 14:06:19 +0300

    Add App Structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import Popup from "../Pop-up/Popup";
 
-function Header() {
-    const [popupOpened, setPopupOpened] = React.useState(false);
-    return (
-        <header>
-            {popupOpened ? <Popup closePopup = {() => setPopupOpened(false)}/> : null}
-            <div>
-                <h2 className="logo">React-BillBoard</h2>
-            </div>
-            <div className="headerRight">
-                <button onClick={() => setPopupOpened(true)} className="orderBtn">Заказать звонок</button>
-                <a href="#" className="headerIcon">
-                    <img src="/cart.png" alt="card" width={30} height={30} />
-                </a>
-                <a href="#" className="headerIcon">
-                    <img src="/profile.png" alt="card" width={30} height={30} />
-                </a>
-                <span>Мой пррофиль</span>
-            </div>
-        </header>
-    );
-}
+const Header = () => {
+  const [popupOpened, setPopupOpened] = React.useState(false);
+
+  return (
+    <header>
+      {popupOpened ? <Popup closePopup={() => setPopupOpened(false)} /> : null}
+      <div>
+        <h2 className="logo">React-BillBoard</h2>
+      </div>
+      <div className="headerRight">
+        <button onClick={() => setPopupOpened(true)} className="orderBtn">
+          Заказать звонок
+        </button>
+        <a href="#" className="headerIcon">
+          <img src="/cart.png" alt="card" width={30} height={30} />
+        </a>
+        <a href="#" className="headerIcon">
+          <img src="/profile.png" alt="card" width={30} height={30} />
+        </a>
+        <span>Мой пррофиль</span>
+      </div>
+    </header>
+  );
+};
 export default Header;
